0a04dd186d8721bf8bc428bdbbb9f1874e8f9b5a
Rizwana-tech <rizwanaa2327@gmail.com>
2026-08-28 21:10:18 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans = "";
+        for(string str : strs){
+            ans += to_string(str.size()) + "#" + str;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i = 0;
+        int n = s.size();
+        while(i < n){
+            int j = i;
+            while(s[j] != '#') j++;
+            int len = stoi(s.substr(i, j-i));
+            ans.push_back(s.substr(j+1,len));
+
+            i = j+1+len;
+        }
+        return ans;
+    }
+};
